128804cd6bb3755231644c26f55e899409e04c55
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-12-06 08:52:00 +0300

    fix layout of quiz

--- src/table/modal/Quiz.js
+++ src/table/modal/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import Elements from '../data/Elements';
 
@@ -29,35 +29,20 @@ function Quiz(props) {
   return (
     <div>
 
-      <table className="quizBorderNone">
-        <thead>
-          <tr>
-            <th colSpan={3}>
-              <div className="quizTitle">Выберите одно или несколько утверждений, которые являются верными:</div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {elementInfo.test[0].map((item, index) => (
-            <tr>
-              <td>
-                <div className="quizDispayInline">
-                  <button
-                    className={quizCheckboxes.find(check => check === index) === undefined ? "quizCheckboxOff" : "quizCheckboxOn"}
-                    onClick={() => handleCheckboxClicked(index)} />
-                </div>
-              </td>
-              <td className="quizWidth">
-                <div className="quizDispayInline">{index + 1}.</div>
-              </td>
-              <td><div className="quizDispayInlineText">{item.q()}</div></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="quizTitle">Выберите одно или несколько утверждений, которые являются верными:</div>
 
-    </div>
+      {elementInfo.test[0].map((item, index) => (
+        <div className="quizItem">
+          <button
+            className={quizCheckboxes.find(check => check === index) === undefined ? "quizCheckboxOff" : "quizCheckboxOn"}
+            onClick={() => handleCheckboxClicked(index)} />
+          <div className="quizNumber">{index + 1}.</div>
+          <div className="quizQuestion">{item.q()}</div>
+        </div>
+      ))}
+    </div >
   )
 }
 
 export default Quiz;
+
