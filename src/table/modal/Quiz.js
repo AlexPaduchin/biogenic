5820fd9594abcf39729a63be9b5d5b78e08e2904
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-12-06 22:21:04 +0300

    fix - go to next test

--- src/table/modal/Quiz.js
+++ src/table/modal/Quiz.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Elements from '../data/Elements';
 
 import './Modal.css';
+import { nullLiteral } from '@babel/types';
 
 function Quiz(props) {
   const {
@@ -10,6 +11,8 @@ function Quiz(props) {
   } = props;
   const [quizCheckboxes, setQuizCheckboxes] = useState([]);
   const [quizIndex, setQuizIndex] = useState(0);
+
+  const [resultChecking, setResultChecking] = useState(undefined);
 
   const handleCheckboxClicked = (i) => {
     const newQuizCheckboxes = quizCheckboxes.filter(elem => elem !== i);
@@ -19,6 +22,7 @@ function Quiz(props) {
     setQuizCheckboxes(newQuizCheckboxes);
   }
   console.log('Quiz props = ', props);
+  console.log('Quiz resultChecking = ', resultChecking);
 
   const elementInfo = Elements[clickedElement];
 
@@ -26,11 +30,24 @@ function Quiz(props) {
     return <div>Извините. Тесты для этого элемента еще не подготовлены.</div>
   }
   console.log('Quiz elementInfo = ', elementInfo);
-  const testArr = elementInfo.test
+  const testArr = elementInfo.test;
+  let titleMessage;
+  switch (resultChecking) {
+    case undefined:
+      titleMessage = <div className="quizTitle">Выберите одно или несколько утверждений, которые являются верными:</div>
+      break;
+    case true:
+      titleMessage = <div className="quizTitleRight">Верно!</div>
+      break;
+    case false:
+      titleMessage = <div className="quizTitleFalse">Результат не верный.</div>
+      break;
+    default: titleMessage = null;
+      break;
+  }
   return (
     <div>
-      <div className="quizTitle">Выберите одно или несколько утверждений, которые являются верными:</div>
-
+      {titleMessage}
       {testArr[quizIndex].map((item, index) => {
         const isItemOff = quizCheckboxes.find(check => check === index) === undefined;
         return (
@@ -43,25 +60,32 @@ function Quiz(props) {
           </div>
         );
       })}
-      <div className="quizFooter">
+      <div className="quizTitle">
         {`Тест № ${quizIndex + 1} ( Всего тестов: ${testArr.length} )`}
-        {quizCheckboxes.length > 0 &&
-          <button
+        {quizCheckboxes.length > 0 && resultChecking === undefined && (
+          < button
             className="quizButton"
             onClick={() => {
-              const newQuizIndex = (quizIndex + 1 < testArr.length) ? quizIndex + 1 : 0;
-              setQuizIndex(newQuizIndex);
+              const newResultChecking = testArr[quizIndex].every((elem, index) => {
+                const quizAnswer = quizCheckboxes.find(check => check === index) !== undefined;
+                console.log('quizCheckboxes = ', quizCheckboxes);
+                console.log('elem.answer = ', elem.answer);
+                console.log('quizAnswer= ', quizAnswer);
+                return ((elem.answer === true) === quizAnswer);
+              });
+              setResultChecking(newResultChecking);
             }}>
             Проверить ответы
           </button>
-        }
-
-        {/* <button
-          className="quizButton"
-          onClick={() => {
-          }}>
-          {(quizIndex + 1 < testArr.length) ? 'Перейти к следующему тесту' : 'Завершить тест'}
-        </button> */}
+        )}
+        {quizCheckboxes.length > 0 && resultChecking !== undefined && (
+          <button
+            className="quizButton"
+            onClick={() => {
+            }}>
+            {(quizIndex + 1 < testArr.length) ? 'Перейти к следующему тесту' : 'Завершить тест'}
+          </button>
+        )}
 
       </div>
       <div className="quizLine"></div>
